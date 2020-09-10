e7b99e6bf02bddc9a22d145f11a4d7541482270d
JuanCalvache <47004448+JuanCalvache@users.noreply.github.com>
2020-09-09 19:40:11 -0500

    algunos scripts

---
+++ Assets/Scripts/Skill_U.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_U : MonoBehaviour
+{
+    [SerializeField]
+    private string name;
+    [SerializeField]
+    private int power;
+
+    [SerializeField]
+    protected Eaffinity affinity;
+
+    public enum Eaffinity
+    {
+        Fire,
+        Wind,
+        Water,
+        Earth,
+        Light,
+        Dark
+    }
+
+    public string Name { get => name; }
+    public int Power { get => power; }
+    public Eaffinity Affinity { get => affinity; }
+
+}
---
+++ Assets/Scripts/SupportSkill_U.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.XPath;
+using UnityEngine;
+
+public class SupportSkill_U : Skill_U
+{
+    public enum ESuppType
+    {
+        AtkUp,
+        DefUp,
+        SpeedDown,
+    }
+    [SerializeField]
+    private ESuppType suppType;
+    
+    public ESuppType SuppType { get => suppType; }
+     
+    public void SetType(int settedType)
+    {
+        switch (settedType)
+        {
+            case (1):
+                suppType = ESuppType.AtkUp;
+                break;
+            case (2):
+                suppType = ESuppType.DefUp;
+                break;
+            case (3):
+                suppType = ESuppType.SpeedDown;
+                break;
+        }
+    }
+}
